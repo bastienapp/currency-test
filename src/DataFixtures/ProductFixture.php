60656c2e6e216fe0db0612170fc1d86da8c61b36
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-12-21 13:21:08 +0100

    fix prices

--- src/DataFixtures/ProductFixture.php
+++ src/DataFixtures/ProductFixture.php
@@ -8,31 +8,15 @@ use Doctrine\Persistence\ObjectManager;
 
 class ProductFixture extends Fixture
 {
-    const PRODUCTS = [
-        [
-            'name' => 'Test 1',
-            'price' => 2.48,
-        ],
-        [
-            'name' => 'Test 2',
-            'price' => 6.24,
-        ],
-        [
-            'name' => 'Test 3',
-            'price' => 1.47,
-        ],
-        [
-            'name' => 'Test 4',
-            'price' => 12.77,
-        ],
-    ];
+    const MAX_PRODUCTS = 5;
 
     public function load(ObjectManager $manager): void
     {
-        foreach (self::PRODUCTS as $key => $productInfos) {
+        for ($i = 0; $i < self::MAX_PRODUCTS; $i++) {
             $product = new Product();
-            $product->setName($productInfos['name']);
-            $product->setPrice($productInfos['price']);
+            $product->setName("Product $i");
+            $value = rand(100, 10000) / 100;
+            $product->setPrice($value);
             $manager->persist($product);
         }
 
